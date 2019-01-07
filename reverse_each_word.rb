4d1f838012f7eeda708af339c446b2692bf9875a
Aaron Berns <anberns@gmail.com>
2019-01-07 20:42:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,3 @@ def reverse_each_word(str)
   rev = spl.join(" ")
   rev
 end
-
-#Write a method called `reverse_each_word` that takes in a string argument of a sentence and returns that same sentence with each word reversed in place.
-
-#First solve it using `.each` Then utilize the same method using `.collect` to see the difference.
